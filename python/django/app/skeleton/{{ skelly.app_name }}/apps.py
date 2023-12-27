239ebbf9d3665b7876a486dbbbb13a48ff3e50aa
Emerson MX <emersonmx@gmail.com>
2023-12-27 20:13:10 -0300

    Remove prefix from django app

--- python/django/app/skeleton/{{ skelly.app_name }}/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class {{ skelly.app_name | replace(from="_", to=" ") | title | split(pat=" ") | join }}Config(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "{{ skelly.app_name }}"
--- python/django/app/update_skeleton.sh
+++ python/django/app/update_skeleton.sh
@@ -7,7 +7,7 @@ script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 django_version="5.0"
 tmp_dir=$(mktemp -d)
 
-camel_case_app_name='{{ skelly.app_name | replace(from="_", to=" ") | title | split(pat=" ") | join }}'
+camel_case_app_name='{{ app_name | replace(from="_", to=" ") | title | split(pat=" ") | join }}'
 
 git clone --depth 1 --branch "stable/${django_version}.x" https://github.com/django/django "$tmp_dir"
 template_dir="$tmp_dir/django/conf/app_template"
@@ -21,16 +21,14 @@ do
             -i "$f"
     fi
 
-    # Add skelly prefix
     sed -r \
-        -e 's/\{\{ (.*) \}\}/{{ skelly.\1 }}/g' \
-        -e 's/\{\{ skelly.camel_case_app_name \}\}/'"$camel_case_app_name"'/g' \
+        -e 's/\{\{ camel_case_app_name \}\}/'"$camel_case_app_name"'/g' \
         -i "$f"
     mv "$f" "${f%'-tpl'}"
 done
 
 rm -rf "$script_dir/skeleton"
 mkdir -p "$script_dir/skeleton"
-mv "$template_dir" "$script_dir/skeleton/{{ skelly.app_name }}"
+mv "$template_dir" "$script_dir/skeleton/{{ app_name }}"
 
 rm -rf "$tmp_dir"
