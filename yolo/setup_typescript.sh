d8f774bd51e368baa0cdd0153735b55345991ae3
Emerson MX <emersonmx@gmail.com>
2024-07-16 16:51:37 -0300

    [yolo] Update setup ts

--- yolo/setup_typescript.sh
+++ yolo/setup_typescript.sh
@@ -1,26 +1,35 @@
 #!/bin/bash
 
-sks javascript/direnv
-sks javascript/editorconfig
-sks javascript/prettier
+ossuary_path="{{ ossuary_path }}"
+
+skelly -s "${ossuary_path}/javascript/direnv"
+skelly -s "${ossuary_path}/javascript/editorconfig"
+skelly -s "${ossuary_path}/javascript/prettier"
 
 npm init -y
-
 npm pkg set private=true --json
 npm pkg set scripts.build="tsc"
-npm pkg set scripts.start="node ./build/main.js"
+npm pkg set scripts.start="node ./dist/main.js"
 npm pkg set scripts.test="jest"
 
-npm install --save-dev typescript @types/node jest ts-jest @types/jest
+npm install --save-dev \
+    typescript \
+    @types/node \
+    jest \
+    ts-jest \
+    @types/jest
 
 curl "https://raw.githubusercontent.com/tsconfig/bases/main/bases/node-lts.json" |
+    npx strip-json-comments-cli |
     jq 'pick(.compilerOptions)
         | .compilerOptions.rootDir="src/"
-        | .compilerOptions.outDir="build/"' >tsconfig.json
+        | .compilerOptions.outDir="dist/"
+        ' >tsconfig.json
 npx ts-jest config:init
 
 mkdir -p src/
 echo 'console.log("Hello World");' >src/main.ts
+echo 'test("1 + 1 = 2", () => { expect(1 + 1).toBe(2); });' >src/main.spec.ts
 
 prettier --write .
 
