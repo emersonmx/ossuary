95e13cf70dd8583ad6d8662fee02aa1b8e9ca5a4
Emerson MX <emersonmx@gmail.com>
2024-07-24 09:36:53 -0300

    [yolo/nodejs] Update setup.sh

--- yolo/nodejs/skeleton/setup.sh
+++ yolo/nodejs/skeleton/setup.sh
@@ -7,10 +7,12 @@ npm init -y
 npm pkg set private=true --json
 npm pkg set scripts.build="esbuild src/main.js --bundle --outfile=dist/main.js"
 npm pkg set scripts.start="node ./dist/main.js"
+npm pkg set scripts.dev="nodemon src/main.js"
 npm pkg set scripts.test="jest"
 
 npm install --save-dev \
     esbuild \
+    nodemon \
     jest
 
 prettier --write .
