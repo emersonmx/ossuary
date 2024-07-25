7bd0d2793e0b382381c02f5ff229230b23b2eec0
Emerson MX <emersonmx@gmail.com>
2024-07-25 14:13:56 -0300

    [snippets/eslint] Update nodejs config

--- snippets/eslint/nodejs.mjs
+++ snippets/eslint/nodejs.mjs
@@ -1,7 +1,9 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
+import jest from "eslint-plugin-jest";
 
 export default [
   { languageOptions: { globals: globals.node } },
   pluginJs.configs.recommended,
+  { files: ["**/*.spec.js"], ...jest.configs["flat/recommended"] },
 ];
