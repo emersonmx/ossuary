8ce1e9201c3976727bc9fba6dcebf94d6825f954
Emerson MX <emersonmx@gmail.com>
2024-07-25 14:36:32 -0300

    [snippets/eslint] Update typescript config

--- snippets/eslint/typescript.mjs
+++ snippets/eslint/typescript.mjs
@@ -6,5 +6,6 @@ export default [
   { files: ["**/*.{js,mjs,cjs,ts}"] },
   { languageOptions: { globals: globals.node } },
   pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
+  ...tseslint.configs.strict,
+  ...tseslint.configs.stylistic,
 ];
