12d7451fdd19486deba59eafc35ea5b5ab73d3ff
Emerson MX <emersonmx@gmail.com>
2024-07-31 09:52:14 -0300

    [yolo] Move python skeleton to yolo

--- yolo/python/skeleton/setup.sh
+++ yolo/python/skeleton/setup.sh
@@ -1,11 +1,38 @@
 #!/bin/bash
 # shellcheck disable=SC2046
 
-sks python/editorconfig
+cat >.editorconfig <<EOF
+$(skf snippets/editorconfig/base)
+
+$(skf snippets/editorconfig/makefile)
+
+$(skf snippets/editorconfig/markdown)
+
+$(skf snippets/editorconfig/yaml)
+EOF
+
+cat >.pre-commit-config.yaml <<EOF
+$(skf snippets/pre-commit/base.yaml)
+
+$(skf snippets/pre-commit/ruff.yaml)
+
+$(skf snippets/pre-commit/default_lint.yaml)
+
+$(skf snippets/pre-commit/mypy.yaml)
+
+$(skf snippets/pre-commit/vulture.yaml)
+EOF
 
 python -m venv --upgrade-deps .venv
 
 .venv/bin/python -m pip install --upgrade $(skf snippets/python/devdeps)
 
-sks python/direnv
+cat >.envrc <<EOF
+export PYTHONBREAKPOINT=ipdb.set_trace
+export PIP_REQUIRE_VIRTUALENV=true
+
+$(skf snippets/direnv/source_file path=.venv/bin/activate)
+
+$(skf snippets/direnv/dotenv)
+EOF
 direnv allow .
