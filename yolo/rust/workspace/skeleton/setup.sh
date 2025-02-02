50d95da684a81f6c14ee351d1ebb0f96846e19d4
Emerson MX <emersonmx@gmail.com>
2025-02-02 17:41:18 -0300

    [yolo/rust] Add workspace

---
+++ yolo/rust/workspace/skeleton/setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# shellcheck disable=SC2046
+
+cat >.editorconfig <<EOF
+$(skf snippets/editorconfig/base)
+
+$(skf snippets/editorconfig/makefile)
+
+$(skf snippets/editorconfig/markdown)
+EOF
+
+cat >.envrc <<EOF
+$(skf snippets/direnv/dotenv)
+EOF
+direnv allow .
+
+cat >.gitignore <<EOF
+$(curl -L https://www.toptal.com/developers/gitignore/api/rust)
+
+.envrc
+EOF
+
+rm -f "$0"
