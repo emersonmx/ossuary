ad2600d6f83ac0faf84586f04a126335c19eb3b6
Emerson MX <emersonmx@gmail.com>
2025-03-11 13:48:57 -0300

    Use eslint and prettier

--- nodejs/setup.sh
+++ nodejs/setup.sh
@@ -10,13 +10,14 @@ npm pkg set type="module"
 npm pkg set scripts.build="swc src -d dist"
 npm pkg set scripts.start="node ./dist/src/main.js"
 npm pkg set scripts.test="jest"
-npm pkg set scripts.format="biome check --write ."
-npm pkg set scripts.lint="biome check ."
+npm pkg set scripts.format="eslint --fix . && prettier --write ."
+npm pkg set scripts.lint="eslint ."
 
 npm install --save-dev $(skf nodejs/devdeps)
 
 shskf editorconfig/nodejs.sh
-skf biome/biome.json >biome.json
+skf prettier/prettier.config.mjs >prettier.config.mjs
+skf eslint/nodejs.mjs >eslint.config.mjs
 skf jest/swc-jest.config.mjs >jest.config.mjs
 
 shskf direnv/nodejs.sh
