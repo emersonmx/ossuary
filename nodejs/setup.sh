d5278c6f39a2297772bc5eb14ad6c579098f01c4
Emerson MX <emersonmx@gmail.com>
2025-03-11 13:58:17 -0300

    [nodejs] Fix setup.sh to use jest

--- nodejs/setup.sh
+++ nodejs/setup.sh
@@ -17,7 +17,7 @@ npm install --save-dev $(skf nodejs/devdeps)
 
 shskf editorconfig/nodejs.sh
 skf prettier/prettier.config.mjs >prettier.config.mjs
-skf eslint/nodejs.mjs >eslint.config.mjs
+skf eslint/nodejs.mjs | sed -E 's/(globals\.node)/\1, ...globals.jest/' >eslint.config.mjs
 skf jest/swc-jest.config.mjs >jest.config.mjs
 
 shskf direnv/nodejs.sh
