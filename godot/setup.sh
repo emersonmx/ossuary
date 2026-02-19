ecf3865274280f6827465b24ef8a4d88a7897987
Emerson MX <emersonmx@gmail.com>
2026-02-19 18:09:57 -0300

    godot: update setup.sh

--- godot/setup.sh
+++ godot/setup.sh
@@ -4,6 +4,10 @@ git init
 shskf gitignore/godot.sh
 
 touch project.godot
+curl \
+    -L \
+    -o icon.svg \
+    https://raw.githubusercontent.com/godotengine/godot/refs/heads/master/editor/icons/DefaultProjectIcon.svg
 
 shskf editorconfig/godot.sh
 skf justfile/godot >justfile
