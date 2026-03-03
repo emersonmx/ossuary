db0421775b73dd80254bf7eb7da0cd64bc09093f
Emerson MX <emersonmx@gmail.com>
2026-03-03 15:04:02 -0300

    nestjs: fix e2e tests

--- nestjs/setup.sh
+++ nestjs/setup.sh
@@ -218,21 +218,30 @@ EOF
 
 cat >test/app.e2e-spec.ts <<'EOF'
 import { Test, TestingModule } from "@nestjs/testing";
-import { INestApplication } from "@nestjs/common";
 import request from "supertest";
-import { App } from "supertest/types";
 import { AppModule } from "./../src/app.module";
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from "@nestjs/platform-fastify";
 
 describe("AppController (e2e)", () => {
-  let app: INestApplication<App>;
+  let app: NestFastifyApplication;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
-    app = moduleFixture.createNestApplication();
+    app = moduleFixture.createNestApplication<NestFastifyApplication>(
+      new FastifyAdapter(),
+    );
     await app.init();
+    await app.getHttpAdapter().getInstance().ready();
+  });
+
+  afterEach(async () => {
+    await app.close();
   });
 
   it("/ (GET)", () => {
