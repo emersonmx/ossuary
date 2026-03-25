76dd57356c163d3d50b00bc7ce303165cdf5a028
Emerson MX <emersonmx@gmail.com>
2026-03-25 11:55:19 -0300

    django: add setup script

---
+++ django/setup.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2046
+
+{% if project_name is matching("[^0-9a-zA-Z_]") -%}
+echo "Project name must only contain letters, numbers, and underscores."
+exit 1
+{%- else -%}
+
+git init
+shskf gitignore/python.sh
+
+uv init
+uv add --upgrade $(skf django/deps)
+uv add --upgrade --dev $(skf python/devdeps)
+uv add --upgrade --dev $(skf django/devdeps)
+
+shskf editorconfig/python.sh
+shskf justfile/python/django/setup.sh >justfile
+
+shskf direnv/python.sh
+direnv allow
+
+# add files
+mkdir -p {{ project_name }}
+skf django/manage.py >manage.py project_name={{project_name}}
+skf django/asgi.py >{{project_name}}/asgi.py project_name={{project_name}}
+skf django/settings.py >{{project_name}}/settings.py project_name={{project_name}}
+skf django/urls.py >{{project_name}}/urls.py project_name={{project_name}}
+skf django/wsgi.py >{{project_name}}/wsgi.py project_name={{project_name}}
+
+just format
+
+{%- endif %}
