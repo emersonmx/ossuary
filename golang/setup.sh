776cd9c4e8ecdc102a3610f6ab07e418ea281d0a
Emerson MX <emersonmx@gmail.com>
2026-04-02 09:44:32 -0300

    golang: update setup.sh

--- golang/setup.sh
+++ golang/setup.sh
@@ -1,15 +1,10 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2046
 
-project_name=$(basename "$PWD")
-module_path="example.com/$project_name"
-gh_username="$(git config github.user)"
-[[ -n "$gh_username" ]] && module_path="github.com/$gh_username/$project_name"
-
 git init
 shskf gitignore/golang.sh
 
-go mod init "$module_path"
+go mod init {{ module_path | default(value='example.com/$(basename "$PWD")') }}
 
 shskf editorconfig/golang.sh
 skf justfile/golang >justfile
