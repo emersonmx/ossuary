caea947ecdc11049e576a5e7d7f4ddc9b7784ab5
Emerson MX <emersonmx@gmail.com>
2026-04-29 14:10:46 -0300

    bevy: add setup.sh script

---
+++ bevy/setup.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2046
+
+{% set git = git | default(value="yes") %}
+{%- if git == "yes" -%}
+git init
+shskf gitignore/rust.sh
+{%- endif %}
+
+cargo init --bin --vcs=none
+cargo add anyhow thiserror bevy
+cargo add log --features max_level_debug,release_max_level_warn
+cargo add --dev rstest
+
+shskf editorconfig/rust.sh
+skf justfile/bevy >justfile
+
+skf env/rust >.env
+skf direnv/dotenv >.envrc
+direnv allow
+
+mkdir -p .cargo/
+cat >.cargo/config.toml <<'EOF'
+[target.x86_64-unknown-linux-gnu]
+linker = "clang"
+rustflags = ["-C", "link-arg=-fuse-ld=lld"]
+EOF
+
+sed -E 's/^log = \{ version = "[0-9.]+",/log = { version = "*",/' -i Cargo.toml
+cat >>Cargo.toml <<'EOF'
+
+[profile.dev]
+opt-level = 1
+
+[profile.dev.package."*"]
+opt-level = 3
+
+[profile.dev.package.wgpu-types]
+debug-assertions = false
+
+[profile.release]
+codegen-units = 1
+lto = "thin"
+
+[profile.wasm-release]
+inherits = "release"
+opt-level = "s"
+strip = "debuginfo"
+EOF
+rm -f Cargo.lock
+
+cat >src/main.rs <<'EOF'
+use bevy::prelude::*;
+
+fn main() {
+    App::new().add_plugins(DefaultPlugins).run();
+}
+EOF
+
+just format
