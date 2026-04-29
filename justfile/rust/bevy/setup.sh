3c79846af091a40ee86d541db29642cc74553381
Emerson MX <emersonmx@gmail.com>
2026-04-29 14:21:21 -0300

    justfile: remove bevy and rust templates

---
+++ justfile/rust/bevy/setup.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+cat >justfile <<EOF
+$(skf justfile/rust/build)
+
+$(skf justfile/rust/bevy/run)
+
+$(skf justfile/rust/watch)
+
+$(skf justfile/rust/format)
+
+$(skf justfile/rust/lint)
+
+$(skf justfile/rust/lint-fix)
+
+$(skf justfile/rust/ci)
+
+$(skf justfile/rust/test)
+
+$(skf justfile/rust/clean)
+EOF
