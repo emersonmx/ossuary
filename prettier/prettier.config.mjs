78f8e39e32c167e38681948dd926dc239ea11690
Emerson MX <emersonmx@gmail.com>
2026-08-14 10:50:42 -0300

    prettier: add tailwindcss plugin option

--- prettier/prettier.config.mjs
+++ prettier/prettier.config.mjs
@@ -1,11 +1,20 @@
+{%- set plugins = [] -%}
+
+{%- if tailwindcss and tailwindcss == "yes" -%}
+{%- set plugins = [...plugins, "prettier-plugin-tailwindcss"] -%}
+{%- endif -%}
+
 /**
  * @see https://prettier.io/docs/en/configuration.html
  * @type {import("prettier").Config}
  */
 const config = {
-    trailingComma: "all",
-    semi: {{ semi | default(value=true) }},
-    singleQuote: false,
+  trailingComma: "all",
+  semi: {{ semi | default(value=true) }},
+  singleQuote: false,
+  {%- if plugins | length > 0 %}
+  plugins: {{ plugins | json_encode }},
+  {%- endif %}
 };
 
 export default config;
