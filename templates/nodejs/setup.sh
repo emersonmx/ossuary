e78ac71940f536d900195c4311a95a8c2e7636f6
Emerson MX <emersonmx@gmail.com>
2026-09-17 19:50:32 -0300

    nodejs: use package.json template in setup.sh

--- templates/nodejs/setup.sh
+++ templates/nodejs/setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2046
 
-shskf pnpm/init.sh project_name="{{ project_name | default(value="$(basename $PWD)") }}"
-
-pnpm add --save-dev --ignore-scripts \
-    $(skf nodejs/deps)
+skf nodejs/package.json \
+    name="{{ project_name | default(value="$(basename $PWD)") }}" \
+    devdeps=$(skf nodejs/deps | tr '\n' ',') \
+    >package.json
