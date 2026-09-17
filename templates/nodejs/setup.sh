46a6ddfcc098ba67058cee0825a33031539a9290
Emerson MX <emersonmx@gmail.com>
2026-09-17 13:33:19 -0300

    nodejs: use pnpm init.sh

--- templates/nodejs/setup.sh
+++ templates/nodejs/setup.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2046
 
-pnpm init
-pnpm pkg set \
-    name="{{ project_name | default(value="$(basename $PWD)") }}"
+shskf pnpm/init.sh project_name="{{ project_name | default(value="$(basename $PWD)") }}"
 
 pnpm add --save-dev --ignore-scripts \
     $(skf nodejs/deps)
