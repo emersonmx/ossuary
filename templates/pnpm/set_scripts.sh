fbb08e50104846d00a9f61246a7999ffa969a4e0
Emerson MX <emersonmx@gmail.com>
2026-09-17 17:44:17 -0300

    pnpm: update set_scripts.sh to remove previous script

--- templates/pnpm/set_scripts.sh
+++ templates/pnpm/set_scripts.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2046
 
+pnpm pkg delete \
+    scripts.build \
+    scripts.start \
+    scripts.test \
+    scripts.format \
+    scripts.lint
 pnpm pkg set \
     scripts.build="{{ build }}" \
     scripts.start="{{ start }}" \
